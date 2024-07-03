edf8b97fc0210078a236c2b910b89de794f0e14f
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-07-03 11:19:25 -0600

    Update background

--- src/Components/Tower/Tower.jsx
+++ src/Components/Tower/Tower.jsx
@@ -6,11 +6,9 @@ import towerImage from "../../assets/cone.png";
 export default function Tower({disks}) {
     return (
         <div className={styles.tower}>
-            <img src={towerImage} alt="tower" />
             {disks.slice().reverse().map((disk) =>
                 <Disk key={disk.id} diskSrc={disk.image} id={disk.id} />
             )}
-            {/* <img src="/src/assets/cone.png" alt="tower" /> */}
         </div>
     );
 }
